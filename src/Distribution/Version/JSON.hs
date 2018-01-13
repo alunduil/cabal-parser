2dfd956db6ddb9fe03ba467b1dcdd20b0c748a44
Alex Brandt <alunduil@alunduil.com>
2018-01-13 13:25:02 -0600

    add example tests for version encoding

--- src/Distribution/Version/JSON.hs
+++ src/Distribution/Version/JSON.hs
@@ -4,7 +4,9 @@ module Distribution.Version.JSON where
 
 import Data.Aeson (ToJSON (toJSON), Value (String))
 import Data.Text (pack)
+import Distribution.Text (disp)
 import Distribution.Version (simplifyVersionRange, VersionRange)
+import Text.PrettyPrint (render)
 
 instance ToJSON VersionRange where
-  toJSON = String . pack . show . simplifyVersionRange
+  toJSON = String . pack . render . disp . simplifyVersionRange
---
+++ test/Distribution/Version/JSONSpec.hs
@@ -0,0 +1,19 @@
+{-# OPTIONS_GHC -fno-warn-deprecations #-}
+
+module Distribution.Version.JSONSpec (main, spec) where
+
+import Data.Aeson (encode)
+import Data.ByteString.Lazy.Char8 (pack)
+import Distribution.Version
+import Distribution.Version.JSON ()
+import Test.Hspec (context, describe, hspec, it, shouldBe, Spec)
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec =
+  describe "ToJSON" $
+    context "encodes examples" $
+      do let s = "\"==1.1.*\"" in it s $ encode (WildcardVersion $ mkVersion [1,1]) `shouldBe` pack s
+         let s = "\">=4.9 && <4.11\"" in it s $ encode (IntersectVersionRanges (UnionVersionRanges (ThisVersion $ mkVersion [4,9]) (LaterVersion $ mkVersion [4,9])) (EarlierVersion $ mkVersion [4,11])) `shouldBe` pack s
