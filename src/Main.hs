5820a08ed5df03b3b7644998cecdb89dda234dc0
Alex Brandt <alunduil@alunduil.com>
2018-01-16 15:29:17 -0600

    add stdout logging

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,13 @@ module Main (main) where
 import API
 import Environment
 import Network.Wai.Handler.Warp (run)
+import Network.Wai.Middleware.RequestLogger (logStdout)
 import Servant (Application, Proxy (Proxy), serve)
 import System.Envy (decodeEnv)
 
 main :: IO ()
-main = either fail (flip run application . port) =<< decodeEnv
+main = either fail (flip run application' . port) =<< decodeEnv
+  where application' = logStdout application
 
 application :: Application
 application = serve (Proxy :: Proxy API) server
